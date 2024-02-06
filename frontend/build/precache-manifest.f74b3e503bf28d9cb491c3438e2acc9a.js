cc515bcd71804a608fa4f415ef4cfb859d79d220
Afiya <afiyaborkar34@gmail.com>
2024-02-06 17:10:55 +0530

    Project Added

---
+++ frontend/build/precache-manifest.f74b3e503bf28d9cb491c3438e2acc9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aacefb005d0ba8c08e1ac620fa175d87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df0c18be4fc1f11786c0",
+    "url": "/static/css/main.6fc34e42.chunk.css"
+  },
+  {
+    "revision": "75b6ca78b0f7dd222b3d",
+    "url": "/static/js/2.13ed2b2c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.13ed2b2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df0c18be4fc1f11786c0",
+    "url": "/static/js/main.59fe0e81.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
